fb26b3528c5670b57c0b08a6778ad63beff793b5
Nathanrods <nathanrod.ads@gmail.com>
2026-04-17 13:29:43 -0300

    teste back

--- server.js
+++ server.js
@@ -1,33 +1,3 @@
-import express from "express";
-import fetch from "node-fetch";
-import cors from "cors";
-
-const app = express();
-app.use(cors());
-
-const key = process.env.AZURE_KEY; // 🔐 seguro
-const region = "brazilsouth";
-
-app.get("/token", async (req, res) => {
-    try {
-        const response = await fetch(
-            `https://${region}.api.cognitive.microsoft.com/sts/v1.0/issueToken`,
-            {
-                method: "POST",
-                headers: {
-                    "Ocp-Apim-Subscription-Key": key
-                }
-            }
-        );
-
-        const token = await response.text();
-        res.json({ token, region });
-
-    } catch (err) {
-        res.status(500).send("Erro ao gerar token");
-    }
+app.get("/", (req, res) => {
+  res.send("Backend SomDaVoz rodando 🚀");
 });
-
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => console.log("Servidor rodando"));
