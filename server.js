b05c0f1f67d0a371da063be7efb3e891d2db3a2b
Nathanrods <nathanrod.ads@gmail.com>
2026-04-17 13:58:35 -0300

    corrigido

--- server.js
+++ server.js
@@ -1,3 +1,14 @@
+import express from "express";
+
+const app = express();
+
+// rota raiz
 app.get("/", (req, res) => {
-  res.send("Backend SomDaVoz rodando 🚀");
+  res.send("API SomDaVoz funcionando 🚀");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Servidor rodando na porta " + PORT);
 });
